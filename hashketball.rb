dfd797d6b76b28a80be85365eb716dcc33387590
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-04 06:30:00 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,48 @@
 # Write your code here!
-
-
+def hashketball
+  game_hash = {
+    :home => {
+   :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => {
+        :number => {},
+        :shoe => {},
+        :points => {},
+        :rebounds => {},
+        :assists => {},
+        :steals => {},
+        :blocks => {},
+        :slam_dunks => {}
+      },
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => {
+        :number => {},
+        :shoe => {},
+        :points => {},
+        :rebounds => {},
+        :assists => {},
+        :steals => {},
+        :blocks => {},
+        :slam_dunks => {}
+      }
+    }
+  } 
+  # num_points_scored = {}
+  
+  # shoe_size = {}
+  
+  # team_names = {}
+  
+  # player_numbers = {}
+  
+  # players_stats = {}
+  
+  # big_shoe_rebounds = {}
+  
+end
 
 
 
